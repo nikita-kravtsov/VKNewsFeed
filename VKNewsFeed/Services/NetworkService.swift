792ad842861c3111db6633be7fc4d65f143e70d4
nikita-kravtsov <payneblr@gmail.com>
2020-06-15 20:42:43 +0300

    Fetch newsfeed JSON

--- VKNewsFeed/Services/NetworkService.swift
+++ VKNewsFeed/Services/NetworkService.swift
@@ -8,33 +8,45 @@
 
 import Foundation
 
-class NetworkService {
+protocol Networking {
+    func request(path: String, params: [String: String], completion: @escaping (Data?, Error?) -> Void)
+}
+
+class NetworkService: Networking {
     
     private let authService: AuthService
     init(authService: AuthService = SceneDelegate.shared().authService) {
         self.authService = authService
     }
     
-    
-    // creating web-adress
-    func getFeed() {
-        
-        var components = URLComponents()
+    func request(path: String, params: [String : String], completion: @escaping (Data?, Error?) -> Void) {
         
         guard let token = authService.token else { return }
-        let params = ["filters": "post, photo"]
+        
         var allParams = params
         allParams["access_token"] = token
         allParams["v"] = API.version
         
+        let url = self.url(path: path, params: allParams)
+        let session = URLSession.init(configuration: .default)
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+        task.resume()
+        print(url)
+    }
+    
+    private func url(path: String, params: [String: String]) -> URL {
+        var components = URLComponents()
+        
         components.scheme = API.scheme
         components.host = API.host
         components.path = API.newsFeed
-        components.queryItems = allParams.map { URLQueryItem(name: $0, value: $1) }
+        components.queryItems = params.map { URLQueryItem(name: $0, value: $1) }
         
-        let url = components.url!
-        print(url)
-        
+        return components.url!
     }
-    
 }
